9514f66866520340e735dbe18c3f4f66d3108bc3
joeabsoftdev <joe@abantech.net>
2016-09-25 08:22:20 -0400

    Added HandJoint.cpp

---
+++ Core/CPP/HandJoint.cpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "HandJoint.h"
+#include "Vector3.h"
+
+#if COMPILING_DLL
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllimport)
+#endif
+
+
+namespace Efficio
+{
+
+	HandJoint::HandJoint(Vector3 position):Position(position)
+	{
+	}
+
+
+	HandJoint::~HandJoint()
+	{
+	}
+}
