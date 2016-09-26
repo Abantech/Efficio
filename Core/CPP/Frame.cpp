f040ea35ad0bb922336617bdf6f63261138447d9
joeabsoftdev <joe@abantech.net>
2016-09-26 04:34:08 -0400

    Additions for Monday. Options and apparent needs are in comments. Working on finalizing

--- Core/CPP/Frame.cpp
+++ Core/CPP/Frame.cpp
@@ -1,9 +1,56 @@
 #include "Frame.h"
+#include "HandJoint.h"
+//#include "Leap.h"
 
-Efficio::Frame::Frame() {
+
+Efficio::Frame::Frame() { //constructor
+	//There seem to be several options depending on the mechanics and preferences of generating variables from sensor input data
+	//Leap.h likely needed for input data specific to each HandJoint
+	//option: iterate for P values from array or other input data structure
+
+	//DIP lh2PIP(Position(position), P, shared_ptr, *P);
+	DIP lh3PIP(*P);
+	DIP lh4PIP(*P);
+	DIP lh5PIP(*P);
+
+	DIP rh2PIP(*P);
+	DIP rh3PIP(*P);
+	DIP rh4PIP(*P);
+	DIP rh5PIP(*P);
+
+	PIP lh2PIP(*P);
+	PIP lh3PIP(*P);
+	PIP lh4PIP(*P);
+	PIP lh5PIP(*P);
+
+	PIP rh2PIP(*P);
+	PIP rh3PIP(*P);
+	PIP rh4PIP(*P);
+	PIP rh5PIP(*P);
+
+	MCP lh1MCP(*P);
+	MCP lh2MCP(*P);
+	MCP lh3MCP(*P);
+	MCP lh4MCP(*P);
+	MCP lh5MCP(*P);
+
+	MCP rh1MCP(*P);
+	MCP rh2MCP(*P);
+	MCP rh3MCP(*P);
+	MCP rh4MCP(*P);
+	MCP rh5MCP(*P);
+
+	CMC lh1CMC(*P);
+	CMC rh1CMC(*P);
+}
+
+Efficio::Frame::~Frame() { //destructor
 
 }
 
-Efficio::Frame::~Frame() {
+/*sample main() below - might be pasted into MainPage.cpp
+*ptr1
+//P should be received from Vector3 for each (for_each, for loop, and/or while loop) HandJoint
 
-}
+//To call any member function: ptr1->memberfunction;
+*/
--- Core/CPP/HandJoint.h
+++ Core/CPP/HandJoint.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector3.h"
+#include "Enumerations.h"
 
 #if COMPILING_DLL
 #define DLLEXPORT __declspec(dllexport)
@@ -7,13 +8,23 @@
 #define DLLEXPORT __declspec(dllimport)
 #endif
 
-//every time the Efficio frame gets created, make the hand joint positions updated
-//call
-//use Position(position)
-//use shared_ptr
-//
+//R: every time the Efficio frame gets created, make the hand joint positions updated
+	//S: view constructor in Frame.cpp
+//R: call
+//R: use Position(position)
+//R: use shared_ptr
 
-
+//S: Top-down organization:
+//lh, rh
+//lh1, lh2, lh3, lh4, lh5; rh1, rh2, rh3, rh4, rh5
+//joints of the hand: fingers 2 through 5 (nonthumbs)
+	//Distal interphalangeal: DIP
+	//Proximal interphilangeal: PIP
+	//Metacarpophalangeal: MCP
+//joints of the hand: finger 1 (thumb)
+	//Interphalangeal: IP
+	//Metacarpophalangeal: MCP
+	//Carpometaparpal: CMC
 
 namespace Efficio 
 {
@@ -25,4 +36,39 @@ namespace Efficio
 			//P is position
 		~HandJoint();
 	};
-}
+	extern class DLLEXPORT DIP : public HandJoint
+	{
+	public:
+		DIP(Vector3 position);
+		Vector3 Position;
+		~DIP();
+	};
+	extern class DLLEXPORT PIP : public HandJoint
+	{
+	public:
+		PIP(Vector3 position);
+		Vector3 Position;
+		~PIP();
+	};
+	extern class DLLEXPORT MCP : public HandJoint
+	{
+	public:
+		MCP(Vector3 position);
+		Vector3 Position;
+		~MCP();
+	};
+	extern class DLLEXPORT IP : public HandJoint
+	{
+	public:
+		IP(Vector3 position);
+		Vector3 Position;
+		~IP();
+	};
+	extern class DLLEXPORT CMC : public HandJoint
+	{
+	public:
+		CMC(Vector3 position);
+		Vector3 Position;
+		~CMC();
+	};
+	}
