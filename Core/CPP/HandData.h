eeab92df78de3c0e8949768baa160413051b8597
james <james@LAPTOP-TH8CI93K>
2016-11-21 20:07:24 -0500

    Created a HandData class that contains all the hands in the scene

---
+++ Core/CPP/HandData.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Data.h"
+#include "Hand.h"
+#include <vector>
+
+#if COMPILING_DLL
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllimport)
+#endif
+
+namespace Efficio
+{
+	namespace Data
+	{
+		namespace Body
+		{
+			extern class DLLEXPORT HandData : public Data
+			{
+			public:
+				HandData();
+				HandData(std::vector<Efficio::Models::Body::Hand> hands);
+				~HandData();
+
+				const std::vector<Efficio::Models::Body::Hand> Hands;
+
+				/// <summary>
+				/// Gets the type of the data.
+				/// </summary>
+				/// <returns></returns>
+				Efficio::Data::DataType GetDataType() { return Efficio::Data::DataType::Hand; }
+			};
+		}
+	}
+}
