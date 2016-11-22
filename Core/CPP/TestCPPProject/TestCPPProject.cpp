79ba0cd639554d8d3df16a62fab6aca80f596e5a
james <james@LAPTOP-TH8CI93K>
2016-11-21 20:03:21 -0500

    Altered TestCPPProject to detect pinch using Efficio Hand Model

--- Core/CPP/TestCPPProject/TestCPPProject.cpp
+++ Core/CPP/TestCPPProject/TestCPPProject.cpp
@@ -2,7 +2,9 @@
 //
 
 #include "Engine.h"
-#include "Pinch.h"
+#include "HandData.h"
+#include <iostream>
+
 
 int main()
 {
@@ -12,9 +14,29 @@ int main()
 
 	while (true)
 	{
-		auto ev = engine.GetFrame()->GetEvents()[0];
-		
+		auto frame = engine.GetFrame();
+		auto data = frame->GetDataOfType(Efficio::Data::DataType::Hand);
 
+		if (data.size() > 0)
+		{
+			auto handData = dynamic_cast<Efficio::Data::Body::HandData*>(data.at(0).get());
+
+			if (handData->Hands.size() > 0)
+			{
+				auto hands = handData->Hands;
+
+				for (size_t i = 0; i < hands.size(); i++)
+				{
+					auto hand = hands[i]; 
+					if (hand.Fingers[0].Tip.DistanceTo(hand.Fingers[1].Tip) < 25)
+					{
+						auto side = (hand.Side == Efficio::Models::Body::BodySide::Left ? "left" : "right");
+						std::cout << side;
+						std::cout << +" pinch\r\n";
+					}
+				}
+			}
+		}
 	}
 }
 
