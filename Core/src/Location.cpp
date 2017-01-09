73c5b0355879fa773f0a0982b96b57aed9d70b7e
James Stephens <james.h.stephens@protonmail.com>
2017-01-09 17:47:05 -0500

    Made ThrowIfAble function

--- Core/src/Location.cpp
+++ Core/src/Location.cpp
@@ -2,25 +2,31 @@
 #include <stdexcept>
 #include<stdio.h>
 
+#if _WINDLL
+#define ThrowIfAble(x) throw x
+#else
+#define ThrowIfAble(x)
+#endif
+
 Efficio::Data::Positional::Location::Location(float latitude, float longitude) : latitude(latitude), longitude(longitude)
 {
 	if (latitude < -180)
 	{
-		//throw std::invalid_argument("Latitude argument cannot be less than -180");
+		ThrowIfAble(std::invalid_argument("Latitude argument cannot be less than -180"));
 	}
 
 	if (latitude > 180)
 	{
-		//throw std::invalid_argument("Latitude argument cannot be more than 180");
+		ThrowIfAble(std::invalid_argument("Latitude argument cannot be more than 180"));
 	}
 
 	if (longitude < -90)
 	{
-		//throw std::invalid_argument("Longitude argument cannot be less than -90");
+		ThrowIfAble(std::invalid_argument("Longitude argument cannot be less than -90"));
 	}
 
 	if (longitude > 90)
 	{
-		//throw std::invalid_argument("Longitude argument cannot be more than 90");
+		ThrowIfAble(std::invalid_argument("Longitude argument cannot be more than 90"));
 	}
 }
