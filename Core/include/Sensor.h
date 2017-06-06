177b9de053e92c40444a1a34ea497c80797250a8
James <james.h.stephens@protonmail.com>
2017-06-06 18:44:22 -0400

    Added sensor

--- Core/include/Sensor.h
+++ Core/include/Sensor.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include "Frame.h"
+#include "SensorStatus.h"
+#include "SensorInformation.h"
 
 namespace Efficio
 {
@@ -11,9 +14,15 @@ namespace Efficio
 		Sensor();
 		~Sensor() {};
 
-		// Virtual Methods
 		/// The type of data the device is able to track
-		virtual std::string Type() { return "type"; };
+		virtual std::string TrackingType() = 0;
+
+		// Virtual Methods
+		/// Connects the device
+		virtual Frame Connect();
+
+		/// Disconnects the device
+		virtual Frame Disconnect();
 
 		/// The unique ID of the device
 		std::string ID;
@@ -22,10 +31,32 @@ namespace Efficio
 		bool Enabled;
 
 		/// The status of the device
-		std::string GetStatus();
+		SensorStatus GetStatus();
+
+		/// Gets the current frame from the device
+		Efficio::Frame GetFrame();
 
 		virtual std::string GetSource();
 
+		virtual SensorInformation GetSensorInformation();
+
+	protected:
+		Frame LastEfficioFrame;
+		SensorInformation SensorInformation;
+		void SetStatus(SensorStatus status);
+
+		virtual std::vector<std::shared_ptr<Data>> GetData() = 0;
+		virtual std::vector<std::shared_ptr<Event>> GetEvents() = 0;
+		/// A Boolean indicating whether or not the device is connected and ready to feed data into Efficio
+		virtual bool IsConnected() = 0;
+		/// A Boolean indicating whether or not the device has a new frame for Efficio
+		virtual bool HasFrame() = 0;
+		virtual void PreGetFrame() = 0;
+		virtual void PostGetFrame() = 0;
+
+
 	private:
+		SensorStatus status;
+		bool connectionStatusChanged;
 	};
 }
--- Core/src/Sensor.cpp
+++ Core/src/Sensor.cpp
@@ -1,16 +1,135 @@
 #include "..\include\Sensor.h"
+#include "..\include\Connected.h"
+#include "..\include\Connecting.h"
+#include "..\include\Disconnected.h"
+#include "..\include\Disconnecting.h"
+#include "..\include\Faulted.h"
 
-namespace Efficio {
-	Sensor::Sensor()
+namespace Efficio
+{
+	Sensor::Sensor() : status(SensorStatus::Uninitialized)
 	{
+	}
+
+	SensorStatus Sensor::GetStatus()
+	{
+		return status;
+	}
+
+	Frame Sensor::Connect()
+	{
+		Frame frame;
+
+		// Indicate the connection state has changed
+		connectionStatusChanged = true;
+
+		// Set the status to "Connecting"
+		status = SensorStatus::Connecting;
+
+		// Add message that the sensor is connecting
+		frame.AddEvent(std::shared_ptr<Event>(new Connecting(SensorInformation)));
+
+		return frame;
+	}
+
+	Frame Sensor::Disconnect()
+	{
+		Frame frame;
+
+		// Indicate the connection state has changed
+		connectionStatusChanged = true;
+
+		// Set the status to "Disconnected"
+		status = SensorStatus::Disconnecting;
+
+		// Add message that the sensor is disconnecting
+		frame.AddEvent(std::shared_ptr<Event>(new Disconnecting(SensorInformation)));
+
+		return frame;
+	}
+
+	Frame Sensor::GetFrame()
+	{
+		Efficio::Frame frame;
+
+		PreGetFrame();
+
+		if (IsConnected())
+		{
+			if (status != SensorStatus::Connected)
+			{
+				SetStatus(SensorStatus::Connected);
+			}
+		}
+		else
+		{
+			if (status != SensorStatus::Disconnected)
+			{
+				SetStatus(SensorStatus::Disconnected);
+			}
+		}
+
+		if (connectionStatusChanged)
+		{
+			switch (status)
+			{
+			case SensorStatus::Faulted:
+				frame.AddEvent(std::shared_ptr<Event>(new Faulted(SensorInformation)));
+				break;
+			case SensorStatus::Connected:
+				frame.AddEvent(std::shared_ptr<Event>(new Connected(SensorInformation)));
+				break;
+			case SensorStatus::Disconnected:
+				frame.AddEvent(std::shared_ptr<Event>(new Disconnected(SensorInformation)));
+				break;
+			case SensorStatus::Disabled:
+			case SensorStatus::Uninitialized:
+			case SensorStatus::Unknown:
+			case SensorStatus::Connecting:
+			case SensorStatus::Disconnecting:
+			default:
+				break;
+			}
+
+			connectionStatusChanged = false;
+		}
+
+		// Check if new frame is available. If not, merge previously calculated frame with base frame
+		if (!HasFrame())
+		{
+			frame.AddFrame(LastEfficioFrame);
+		}
+		else
+		{
+			// Create temporary frame
+			Frame tempFrame;
+
+			tempFrame.AddData(GetData());
+			tempFrame.AddEvents(GetEvents());
+
+			LastEfficioFrame = tempFrame;
+
+			frame.AddFrame(tempFrame);
+		}
+
+		PostGetFrame();
+
+		return frame;
 	}
 
 	std::string Sensor::GetSource()
 	{
-		return "source";
+		return SensorInformation.Name;
 	}
 
-	std::string Sensor::GetStatus() {
-		return "status";
+	SensorInformation Sensor::GetSensorInformation()
+	{
+		return SensorInformation;
+	}
+
+	void Sensor::SetStatus(SensorStatus status)
+	{
+		connectionStatusChanged = true;
+		Sensor::status = status;
 	}
 }
