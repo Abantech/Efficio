e1b469a3fc350bd1ab8cc5198031eb4c076159f7
James <james.h.stephens@protonmail.com>
2017-06-19 18:31:44 -0400

    Added proximal and distal joints to the finger joints

--- Plugins/Models/Body/src/Finger.cpp
+++ Plugins/Models/Body/src/Finger.cpp
@@ -9,10 +9,21 @@ namespace Efficio
 			Finger::Finger() : Valid(false), IsExtended(false), Length(-1), FingerType(FingerType::Unknown)
 			{
 			}
+
 			Finger::Finger(Efficio::Models::Body::FingerType fingerType, bool isExtended, float length, Joint tip, Joint dip, Joint mcp, Joint pip)
 				: FingerType(fingerType), IsExtended(isExtended), Length(length), Tip(tip), DIP(dip), MCP(mcp), PIP(pip), Valid(true)
 			{
+				Tip.ProximalJoints.push_back(std::make_shared<Joint>(DIP));
+
+				DIP.DistalJoints.push_back(std::make_shared<Joint>(Tip));
+				DIP.ProximalJoints.push_back(std::make_shared<Joint>(MCP));
+
+				PIP.DistalJoints.push_back(std::make_shared<Joint>(DIP));
+				PIP.ProximalJoints.push_back(std::make_shared<Joint>(MCP));
+
+				MCP.DistalJoints.push_back(std::make_shared<Joint>(PIP));
 			}
+
 			Finger::~Finger()
 			{
 			}
