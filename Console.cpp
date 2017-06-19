1e97b1966fd9a8d144d2255c79c8ce9c651f1251
James <james.h.stephens@protonmail.com>
2017-06-19 18:36:21 -0400

    Console with Message bus experiments

--- Console.cpp
+++ Console.cpp
@@ -1,13 +1,54 @@
 #include "Engine.h"
+#include "PinchEvent.h"
+#include "BodySide.h"
+#include "PointEvent.h"
+#include "Connected.h"
+#include "FingerType.h"
 #include <iostream>
+#include "Message.h"
+
+void PrintOnPinch(Efficio::Message message)
+{
+	auto pinch = dynamic_cast<Efficio::Recognition::Body::PinchEvent*>(message.GetEvent().get());
+	if (pinch->Finger1.FingerType == Efficio::Models::Body::FingerType::Thumb && pinch->Finger2.FingerType == Efficio::Models::Body::FingerType::Index)
+	{
+		cout << "Pinch point: (" << pinch->Midpoint().x() << ", " << pinch->Midpoint().y() << ", " << pinch->Midpoint().z() << ")\n";
+	}
+}
+
+void PrintOnSensorConnected(Efficio::Message message)
+{
+	auto con = dynamic_cast<Efficio::Connected*>(message.GetEvent().get());
+	cout << con->SensorInformation.Name << " Connected";
+}
+
+void PrintOnPoint(Efficio::Message message)
+{
+	auto point = dynamic_cast<Efficio::Recognition::Body::PointEvent*>(message.GetEvent().get());
+	cout << (point->Side == Efficio::Models::Body::BodySide::Right ? "Right" : "Left") << " Point Detected\n";
+}
 
 int main(int argc, char** argv) {
 	Efficio::Engine engine;
+
+	// Register Events
+	engine.MessageBus->AddReceiver(Efficio::Recognition::Body::PinchEvent::EventType, std::function<void(Efficio::Message)>(PrintOnPinch));
+	engine.MessageBus->AddReceiver(Efficio::Connected::EventType, std::function<void(Efficio::Message)>(PrintOnSensorConnected));
+	engine.MessageBus->AddReceiver(Efficio::Recognition::Body::PointEvent::EventType, std::function<void(Efficio::Message)>(PrintOnPoint));
+	
+	// Start your engines!
 	engine.Start();
 
 	while (true)
 	{
-		std::cout << engine.GetFrame()->ID << "\n";
+		// Get Frames
+		auto frame = engine.GetFrame();
+
+		// Magic happens here
+
+		// Release the frame into the wild
+		engine.ReleaseFrame();
 	}
+
 	return 0;
 }
