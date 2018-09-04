d40b8aee38a71d1708752cc42350688e6363bfab
James Stephens <james@codevanguard.com>
2018-09-04 17:21:47 -0300

    Added face class
    Cleaned CmakeList

---
+++ Plugins/Models/Body/include/Face.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Efficio
+{
+	namespace Models
+	{
+		namespace Body
+		{
+			/// Representation of the human face
+			class Face
+			{
+			public:
+				Face();
+				~Face();
+			};
+		}
+	}
+};
